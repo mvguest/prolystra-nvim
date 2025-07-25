5dc84aa9c94649489aa710f0c2ea9a7fd2804e9a
mvg <matheusvictor.noronha@gmail.com>
2025-07-25 04:18:15 -0300

    Initial commit

---
+++ lua/config/options.lua
@@ -0,0 +1,7 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+
+-- TABS CONFIG
+vim.opt.tabstop = 4 -- Número de espaços que uma tab representa
+vim.opt.shiftwidth = 4 -- Número de espaços usados ao usar >> ou <<
---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,93 @@
+return {
+  -- tokyonight
+  {
+    "folke/tokyonight.nvim",
+    lazy = true,
+    opts = { style = "moon" },
+  },
+
+  -- catppuccin
+  {
+    "catppuccin/nvim",
+    lazy = true,
+    name = "catppuccin",
+    opts = {
+      integrations = {
+        aerial = true,
+        alpha = true,
+        cmp = true,
+        dashboard = true,
+        flash = true,
+        grug_far = true,
+        gitsigns = true,
+        headlines = true,
+        illuminate = true,
+        indent_blankline = { enabled = true },
+        leap = true,
+        lsp_trouble = true,
+        mason = true,
+        markdown = true,
+        mini = true,
+        native_lsp = {
+          enabled = true,
+          underlines = {
+            errors = { "undercurl" },
+            hints = { "undercurl" },
+            warnings = { "undercurl" },
+            information = { "undercurl" },
+          },
+        },
+        navic = { enabled = true, custom_bg = "lualine" },
+        neotest = true,
+        neotree = true,
+        noice = true,
+        notify = true,
+        semantic_tokens = true,
+        snacks = true,
+        telescope = true,
+        treesitter = true,
+        treesitter_context = true,
+        which_key = true,
+      },
+    },
+    specs = {
+      {
+        "akinsho/bufferline.nvim",
+        optional = true,
+        opts = function(_, opts)
+          if (vim.g.colors_name or ""):find("catppuccin") then
+            opts.highlights = require("catppuccin.groups.integrations.bufferline").get()
+          end
+        end,
+      },
+    },
+  },
+
+  -- add gruvbox
+  { "ellisonleao/gruvbox.nvim" },
+
+  -- add yoru
+  { "yorumicolors/yorumi.nvim" },
+
+  -- add radium
+  { "Sly-Harvey/radium.nvim" },
+
+  -- add nyoom
+  { "nyoom-engineering/nyoom.nvim" },
+
+  -- add kanagawa
+  { "rebelot/kanagawa.nvim" },
+
+  -- add poimandres
+  { "olivercederborg/poimandres.nvim" },
+
+  -- add no-clown-fiesta
+  { "aktersnurra/no-clown-fiesta.nvim" },
+  -- define colors_name
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "kanagawa-dragon",
+    },
+  },
+}
